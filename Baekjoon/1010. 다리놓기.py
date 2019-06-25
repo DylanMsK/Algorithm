7036e2f6e1006d68fc05802c37782c127b48a865
DylanMsK <kms920612@gmail.com>
2019-06-25 20:48:07 +0900

    백준 1010 다리놓기 끝

--- Baekjoon/1010. 다리놓기.py
+++ Baekjoon/1010. 다리놓기.py
@@ -3,12 +3,11 @@
 T = int(input())
 for _ in range(T):
     N, M = map(int, input().split())
-    
-    k = (M-N)+1
-    tot = 0
-    while k > 0:
-        for i in range(k, 0, -1):
-            tot += i
-        k -= 1
 
+    tot = 1
+    if N != M:
+        for i in range(M, M-N, -1):
+            tot *= i
+        for i in range(N, 1, -1):
+            tot //= i
     print(tot)
