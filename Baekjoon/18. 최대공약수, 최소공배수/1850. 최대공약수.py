95066ac6d80ed47dcbcb75e8bb18192cf3e2bc3c
DylanMsK <kms920612@gmail.com>
2019-07-04 17:50:11 +0900

    아직 못품

--- Baekjoon/18. 최대공약수, 최소공배수/1850. 최대공약수.py
+++ Baekjoon/18. 최대공약수, 최소공배수/1850. 최대공약수.py
@@ -1,7 +1,20 @@
 # url = 'https://www.acmicpc.net/problem/1850'
 
 a, b = map(int, input().split())
-for i in range(2, 8):
+m = min(min(a, b), abs(a-b))
+com = [1]
+nums = [0] + [1]*m
+for i in range(2, int(m**0.5)):
+    if nums[i]:
+        com.append(i)
+        for j in range(i+i, m+1, i):
+            nums[j] = 0
+            com.append(j)
+com.append(m)
+com.sort(reverse=True)
+for i in com:
     if a % i == 0 and b % i == 0:
-        r = i
-print('1' * r)
+        m = i
+        break
+
+print('1'*m)
