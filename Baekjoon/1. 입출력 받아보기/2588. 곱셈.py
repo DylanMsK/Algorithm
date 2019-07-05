bde6df6577661e1133f1162d3a84035c18c26551
DylanMsK <kms920612@gmail.com>
2019-07-05 17:55:29 +0900

    단계별로풀기 하는중

---
+++ Baekjoon/1. 입출력 받아보기/2588. 곱셈.py
@@ -0,0 +1,5 @@
+a = int(input())
+b = input()
+
+for i in [b[-1], b[-2], b[-3], b]:
+    print(a * int(i))
---
+++ Baekjoon/3. for문 사용해보기/11022. A+B - 8.py
@@ -0,0 +1,5 @@
+# url = 'https://www.acmicpc.net/problem/11022'
+
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    print(f'Case #{_+1}: {a} + {b} = {a+b}')
