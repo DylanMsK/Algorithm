e882e8d02a462d587a3a714fa34e5e6138e3b090
DylanMsK <kms920612@gmail.com>
2019-07-08 21:26:44 +0900

    주사위 굴리기 마크다운 작성 끝

--- Baekjoon/16134. 조합 (Combination).py
+++ Baekjoon/16134. 조합 (Combination).py
@@ -1,12 +1,20 @@
 # url = 'https://www.acmicpc.net/problem/16134'
 
+def combination(N, R):
+    res = 1
+    for i in range(N, N-R, -1):
+        res *= i
+
+    for i in range(2, R+1):
+        res //= i
+    return res
+
 N, R = map(int, input().split())
 
-res = 1
-for i in range(N, N-R, -1):
-    res *= i
+res = 0
+for i in range(R-1, N):
+    if res > 1000000007:
+        break
+    res += combination(i, R-1)
 
-for i in range(R, 1, -1):
-    res //= i
-
-print(res % 1000000007)
+print(res)
