2f3a7b05437b5949c092b768bb24a9efcd8d65a1
DylanMsK <kms920612@gmail.com>
2019-07-10 21:31:32 +0900

    마크다운 작성 끝

--- Samsung/14502. 연구소.py
+++ Samsung/14502. 연구소.py
@@ -7,23 +7,27 @@ arr = [list(map(int, input().split())) for _ in range(N)]
 
 viruses = []
 empties = []
+cnt_zeros = 0
 for y in range(N):
     for x in range(M):
         if arr[y][x] == 2:
             viruses.append((x, y))
         elif arr[y][x] == 0:
+            cnt_zeros += 1
             empties.append((x, y))
 
+cnt_zeros -= 3
 max_ = 0
 def spread(viruses, walls):
-    global max_
+    global max_, cnt_zeros
 
+    left = cnt_zeros
     brr = deepcopy(arr)
 
     q = viruses[::]
     for x, y in walls:
         brr[y][x] = 1
-    
+
     while q:
         x, y = q.pop(0)
         for dx, dy in (0, -1), (1, 0), (0, 1), (-1, 0):
@@ -32,17 +36,15 @@ def spread(viruses, walls):
                 continue
             if brr[ny][nx]:
                 continue
+            left -= 1
             brr[ny][nx] = 2
             q.append((nx, ny))
 
-    cnt = 0
-    for y in range(N):
-        for x in range(M):
-            if brr[y][x] == 0:
-                cnt += 1
+        if left <= max_:
+            return
     
-    if cnt > max_:
-        max_ = cnt
+    if left > max_:
+        max_ = left
 
 for idx, walls in enumerate(combinations(empties, 3)):
     spread(viruses, walls)
