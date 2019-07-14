7e302c81fefb320be1643a191177642e8018da3d
MINSU KANG <kms920612@gmail.com>
2019-07-15 00:48:54 +0900

    정답

--- Samsung/14503. 로봇 청소기.py
+++ Samsung/14503. 로봇 청소기.py
@@ -4,11 +4,22 @@ N, M = map(int, input().split())
 r, c, d = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
-def check_clean(nx, ny):
+bfs = {
+    0: [(-1, 0, 3), (0, 1, 2), (1, 0, 1), (0, -1, 0)],
+    1: [(0, -1, 0), (-1, 0, 3), (0, 1, 2), (1, 0, 1)],
+    2: [(1, 0, 1), (0, -1, 0), (-1, 0, 3), (0, 1, 2)],
+    3: [(0, 1, 2), (1, 0, 1), (0, -1, 0), (-1, 0, 3)]
+}
+back = {
+    0: (1, 0),
+    1: (0, -1),
+    2: (-1, 0),
+    3: (0, 1)
+}
+
+def check_clean(x, y):
     global cnt
-    if ny < 0 or ny >= N or nx < 0 or nx >= M:
-        return False
-    if arr[ny][nx]:
+    if y < 0 or y >= N or x < 0 or x >= M or arr[y][x]:
         return False
     cnt += 1
     return True
@@ -19,74 +30,18 @@ cnt = 1
 while 1:
     y, x, d = q.pop(0)
     arr[y][x] = 2
-
-    flag = False
-    if d == 0:
-        for dx, dy, nd in (-1, 0, 3), (0, 1, 2), (1, 0, 1), (0, -1, 0):
-            ny, nx = y+dy, x+dx
-            if check_clean(nx, ny):
-                flag = True
-                q.append((ny, nx, nd))
-                break
-        else:
-            ny = y+1
-            if ny < N:
-                if arr[ny][x] == 1:
-                    break
-                q.append((ny, x, d))
-                continue
-            else:
-                break
-
-    elif d == 1:
-        for dx, dy, nd in (0, -1, 0), (-1, 0, 3), (0, 1, 2), (1, 0, 1):
-            ny, nx = y+dy, x+dx
-            if check_clean(nx, ny):
-                flag = True
-                q.append((ny, nx, nd))
-                break
-        else:
-            nx = x-1
-            if nx >= 0:
-                if arr[ny][x] == 1:
-                    break
-                q.append((y, nx, d))
-                continue
-            else:
-                break
-
-    elif d == 2:
-        for dx, dy, nd in (1, 0, 1), (0, -1, 0), (-1, 0, 3), (0, 1, 2):
-            ny, nx = y+dy, x+dx
-            if check_clean(nx, ny):
-                flag = True
-                q.append((ny, nx, nd))
-                break
-        else:
-            ny = y-1
-            if ny >= 0:
-                if arr[ny][x] == 1:
-                    break
-                q.append((ny, x, d))
-                continue
-            else:
-                break
-
+    for dx, dy, nd in bfs[d]:
+        ny, nx = y+dy, x+dx
+        if check_clean(nx, ny):
+            q.append((ny, nx, nd))
+            break
     else:
-        for dx, dy, nd in (0, 1, 2), (1, 0, 1), (0, -1, 0), (-1, 0, 3):
-            ny, nx = y+dy, x+dx
-            if check_clean(nx, ny):
-                flag = True
-                q.append((ny, nx, nd))
+        ny, nx = y+back[d][0], x+back[d][1]
+        if ny >= 0 or ny < N or nx >= 0 or nx < M:
+            if arr[ny][nx] == 1:
                 break
+            q.append((ny, nx, d))
         else:
-            nx = x+1
-            if nx < M:
-                if arr[ny][x] == 1:
-                    break
-                q.append((y, nx, d))
-                continue
-            else:
-                break
-    
+            break
+
 print(cnt)
