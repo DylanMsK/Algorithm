f252d832c0d635a57f5cb06dc5aa145e6f12d4e1
MINSU KANG <kms920612@gmail.com>
2019-07-30 23:22:58 +0900

    정답

--- Samsung/17142. 연구소 3.py
+++ Samsung/17142. 연구소 3.py
@@ -70,21 +70,38 @@ for y in range(1, N+1):
             promiss.append((x, y))
             arr[y][x] = 0
 
-def dfs(lst, cnt):
-    global max_
+# def bfs(lst, tot, inact):
+#     global max_
 
-    if arr[y][x] or brr[y][x]:
-        max_ = max(max_, cnt)
-        return
-    else:
-        brr[y][x] = 1
-        dfs(x, y-1, cnt+1)
-        dfs(x+1, y, cnt+1)
-        dfs(x, y+1, cnt+1)
-        dfs(x-1, y, cnt+1)
+#     for x, y in lst:
+#         brr[y][x] = 1
 
+#     nxt = []
+#     for x, y in lst:
+#         for dx, dy in (0, -1), (1, 0), (0, 1), (-1, 0):
+#             nx, ny = x+dx, y+dy
+#             if arr[ny][nx] or brr[ny][nx]:
+#                 continue
+#             brr[ny][nx] = 1
+#             nxt.append((nx, ny))
 
+#     if nxt:
+#         if isInactives(nxt):
+#             bfs(nxt, tot, inact+1)
+#         else:
+#             bfs(nxt, tot+inact+1, 0)
+#     else:
+#         max_ = max(max_, tot)
+#         return
 
+def isInactives(nxt):
+    nums = 0
+    for x, y in nxt:
+        if (x, y) in promiss:
+            nums += 1
+    if nums == len(nxt):
+        return True
+    return False
 
 def check():
     for y in range(1, N+1):
@@ -94,22 +111,49 @@ def check():
     return True
 
 
-min_ = 1e9
-flag = False
+# min_ = 1e9
+# flag = False
+# for comb in combinations(promiss, M):
+#     max_ = 0
+#     brr = [[0]*(N+2) for _ in range(N+2)]
+#     bfs(comb, 0, 0)
+
+#     if check():
+#         flag = True
+#         min_ = min(min_, max_)
+
+# print(min_) if flag else print(-1)
+
+min_, flag = 1e9, False
 for comb in combinations(promiss, M):
-    max_ = 0
+    q = comb
     brr = [[0]*(N+2) for _ in range(N+2)]
-    for x, y in comb:
-        print(x, y)
-        dfs(x, y, 0)
+    tot, inact = 0, 0
+    while 1:
+        nxt_q = []
+        for x, y in q:
+            brr[y][x] = 1
+            for dx, dy in (0, -1), (1, 0), (0, 1), (-1, 0):
+                nx, ny = x+dx, y+dy
+                if arr[ny][nx] or brr[ny][nx]:
+                    continue
+                brr[ny][nx] = 1
+                nxt_q.append((nx, ny))
+        
+        if nxt_q:
+            if isInactives(nxt_q):
+                inact += 1
+            else:
+                tot += inact + 1
+                inact = 0
+                if tot >= min_:
+                    break
+            q = nxt_q
+        else:
+            break
+    
     if check():
         flag = True
-        min_ = min(min_, max_)
-    print()
-    print(comb)
-    print(max_)
-    for i in brr:
-        print(i)
-    break
+        min_ = min(min_, tot)
 
 print(min_) if flag else print(-1)
