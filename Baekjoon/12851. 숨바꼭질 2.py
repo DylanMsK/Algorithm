d23d2a78cd958d77714dfac11658f268e1e14d79
강민수 <kms920612@gmail.com>
2019-08-13 17:29:15 +0900

    정답

--- Baekjoon/12851. 숨바꼭질 2.py
+++ Baekjoon/12851. 숨바꼭질 2.py
@@ -3,25 +3,34 @@
 N, K = map(int, input().split())
 
 time = 0
-visited = [0] * (100001)
+visited = [0] * 100001
+cnt = [0] * 100001
+q = [N]
 visited[N] = 1
-present = [N]
+cnt[N] = 1
 while 1:
-    nxt = []
-    # print(time, present, visited)
-    cnt = [0] * 100001
-    for now in present:
-        cnt[now] = 1
-        for j in (-1, 1, now):
-            if 0 <= now+j <= 100000:
-                visited[now+j] += cnt[now]
-                if now+j not in nxt:
-                    nxt.append(now+j)
-    time += 1
-    if K in nxt:
-        # print(time, nxt, cnt)
+    if visited[K]:
         break
-    present = nxt
+        
+    nxt = []
+    for i in q:
+        if i > K:
+            if not visited[i-1]:
+                nxt.append(i-1)
+                cnt[i-1] += cnt[i]
+        else:
+            for j in (-1, 1, i):
+                if 0 <= i+j <= 100001 and not visited[i+j]:
+                    nxt.append(i+j)
+                    cnt[i+j] += cnt[i]
+
+    time += 1
+    nxt = list(set(nxt))
+
+    for i in nxt:
+        visited[i] = 1
+
+    q = nxt
 
 print(time)
-print(visited[K])
+print(cnt[K])
