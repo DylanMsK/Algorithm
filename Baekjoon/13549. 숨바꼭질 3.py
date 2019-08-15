ab0edd04421f85e637461bd5b41f3cf968f8f910
MINSU KANG <kms920612@gmail.com>
2019-08-16 01:18:49 +0900

    정답

--- Baekjoon/13549. 숨바꼭질 3.py
+++ Baekjoon/13549. 숨바꼭질 3.py
@@ -1,37 +1,53 @@
 # url = 'https://www.acmicpc.net/problem/13549'
 
 N, K = map(int,input().split())
-
-visited = [0]*100001
-q = []
-for i in range(N, 100001, N):
-    visited[i] = 1
-    q.append(i)
-time = 0
-while 1:
-    if visited[K]:
-        break
-    nxt = []
-    for i in q:
-        if i > K:
-            if 0 <= i-1 < 100001 and not visited[i-1]:
-                nxt.append(i-1)
-        else:
-            for j in 1, -1:
-                if 0 <= i+j < 100001 and not visited[i+j]:
-                    nxt.append(i+j)
-    
-    nxt = list(set(nxt))
-    time += 1
+if N > K:
+    print(N-K)
+elif N == K:
+    print(0)
+else:
+    time = 0
+    length = min(100001, K*2+1)
+    visited = [0]*length
     q = []
-    for i in nxt:
-        if i == 0:
-            visited[i] = 1
-            continue
-        for j in range(i, 100001, i):
-            if not visited[j]:
-                visited[j] = 1
-                q.append(j)
-    q = list(set(q))
+    if N == 0:
+        visited[0] = 1
+        q.append(0)
+    else:
+        now = N
+        while now < length:
+            visited[now] = 1
+            q.append(now)
+            now *= 2
 
-print(time)
+    while 1:
+        if visited[K]:
+            break
+        nxt = []
+        for i in q:
+            if i > K:
+                if 0 <= i-1 < length and not visited[i-1]:
+                    nxt.append(i-1)
+                continue
+            for j in 1, -1:
+                if 0 <= i+j < length and not visited[i+j]:
+                    nxt.append(i+j)
+        nxt = set(nxt)
+        q = []
+        for i in nxt:
+            if i == 0:
+                visited[0] = 1
+                q.append(0)
+                continue
+            now = i
+            while 1:
+                if 0 <= now < length and not visited[now]:
+                    visited[now] = 1
+                    q.append(now)
+                if now > K:
+                    break
+                now *= 2
+        q = list(set(q))
+        time += 1
+
+    print(time)
