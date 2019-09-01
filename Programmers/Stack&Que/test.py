6d2d0f563a4fbe33404bd2535344629434311c41
MINSU KANG <kms920612@gmail.com>
2019-09-01 14:35:13 +0900

    마크다운 작성 끝

--- Programmers/Stack&Que/test.py
+++ Programmers/Stack&Que/test.py
@@ -1,9 +1,25 @@
-def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x*3, reverse=True)
-    print(numbers)
+def solution(begin, target, words, answer=0):
+    if begin == target:
+        return answer
+    if not words:
+        return 0
+    print(begin, target, words)
+    l = len(begin)
+    promiss = []
+    for word in words:
+        cnt = 0
+        for i in range(l):
+            if cnt > 1:
+                break
+            if word[i] == begin[i]:
+                cnt += 1
+        if cnt == 1:
+            promiss.append(word)
+
+    print(f"answer: {answer}, promiss: {promiss}")
+    while promiss:
+        word = promiss.pop(0)
+        solution(word, target, [i for i in words if i != word], answer+1)
     
-    return str(int(''.join(numbers)))
 
-
-print(solution(['6', '10', '2']))
+print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
