becb806e35680d83eb28a150cc38c2791b8a19a4
MINSU KANG <kms920612@gmail.com>
2019-09-28 15:24:18 +0900

    마크다운 작성 끝

--- Baekjoon/11650. 좌표 정렬하기.py
+++
@@ -1,7 +0,0 @@
-# url = 'https://www.acmicpc.net/problem/11650'
-
-N = int(input())
-locations = [tuple(map(int, input().split())) for _ in range(N)]
-locations.sort(key=lambda x: (x[0], x[1]))
-for i in locations:
-    print(i[0], i[1])
--- Baekjoon/11651. 좌표 정렬하기 2.py
+++
@@ -1,7 +0,0 @@
-# url = 'https://www.acmicpc.net/problem/11651'
-
-N = int(input())
-locations = [tuple(map(int, input().split())) for _ in range(N)]
-locations.sort(key=lambda x: (x[1], x[0]))
-for i in locations:
-    print(i[0], i[1])
