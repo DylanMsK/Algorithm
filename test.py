269bfde8ba0228bcbfa89eef0a9e11ae1022657b
MINSU KANG <kms920612@gmail.com>
2019-10-17 22:44:37 +0900

    정답

--- test.py
+++ test.py
@@ -1,11 +1,22 @@
-def solution(n):
-    if n == 1 or n == 0:
-        return 1
-    else:
-        return solution(n-1) + solution(n-2)
+def solution(N):
+    # write your code in Python 3.6
+    binary = ''
+    while N > 1:
+        if N % 2:
+            binary += '1'
+        else:
+            binary += '0'
+        N //= 2
+    binary += '1'
+    binary = binary[::-1]
+    max_, cnt = 0, 0
+    for b in binary:
+        if b == '1':
+            max_ = max(max_, cnt)
+            cnt = 0
+        else:
+            cnt += 1
+    return max_
 
-print(solution(3))
-print(solution(4))
-print(solution(5))
-print(solution(6))
-print(solution(7))
+N = 1041
+print(solution(N))
